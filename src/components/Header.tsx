726a6f3a706f283f8b37fcf05f0c2e9c53099f91
elenintoulou <elenintoulou@hotmail.com>
2025-12-29 17:11:43 +0200

    feat: create header

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+
+const Header    = () => {
+    return (
+        <>
+            <header className="bg-cf-dark-red w-full">
+                <div>
+                    <img className="my-4 h-16"
+                         src="https://edudz.elearning.aueb.gr/pluginfile.php/1/theme_adaptable/logo/1763733517/dz_logo.jpg"
+                         alt="cf logo"/>
+                    <nav><a href="#">Home</a></nav>
+                </div>
+            </header>
+        </>
+    )
+}
+export default Header;
